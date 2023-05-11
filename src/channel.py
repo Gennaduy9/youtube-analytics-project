773335dd89309253afcfcaf98049f6dd04c2aa11
Геннадий <gubingm@list.ru>
2023-05-11 21:24:39 +0300

    Changed the channel.py function and main.py

--- src/channel.py
+++ src/channel.py
@@ -12,18 +12,11 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        channel_data = self.print_info()
-        self.title = channel_data['items'][0]['snippet']['title']
-        self.description = channel_data['items'][0]['snippet']['description']
-        self.url = 'https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA'
-        self.subscriberCount = channel_data['items'][0]['statistics']['subscriberCount']
-        self.video_count = channel_data['items'][0]['statistics']['videoCount']
-        self.view_count = channel_data['items'][0]['statistics']['viewCount']
 
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        api_key: str = os.getenv('API_KEY')
+        api_key: str = os.getenv('API_KEY1')
         youtube = build('youtube', 'v3', developerKey=api_key)
         result = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
