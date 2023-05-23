61aa8d7c84c45293be4390fbd9baa101190c28c5
Геннадий <gubingm@list.ru>
2023-05-23 15:54:07 +0300

    Создал новый файл video.py в нём реализовал инициализацию реальными данными экземпляра класс Video. Создал новый класс PLVideo на основе уже существующего.

---
+++ src/video.py
@@ -0,0 +1,29 @@
+import os
+from googleapiclient.discovery import build
+
+
+class Video:
+    def __init__(self, video_id: str):
+        self.video_id = video_id
+
+        video_data = self.video_response()
+        self.video_title: str = video_data['items'][0]['snippet']['title']
+        self.video_url: str = f"https://www.youtube.com/watch?v={video_id}"
+        self.view_count: int = video_data['items'][0]['statistics']['viewCount']
+        self.like_count: int = video_data['items'][0]['statistics']['likeCount']
+
+    def video_response(self):
+        api_key: str = os.getenv('API_KEY1')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                     id=self.video_id
+                                     ).execute()
+
+    def __str__(self):
+        return self.video_title
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, pl_id: str):
+        super().__init__(video_id)
+        self.pl_id = pl_id
